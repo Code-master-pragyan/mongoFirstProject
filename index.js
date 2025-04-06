5edb3c0933471c5eb5eba031bd458bcbd4f831e4
Pragyan Lahkar <pragyanlahkar28@gmail.com>
2025-04-06 12:14:08 +0530

    last commit

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const mongoose = require("mongoose");
 const path = require("path");
 const Chat = require("./models/chat.js");
 const methodOverride = require("method-override");
+const ExpressError = require("./ExpressError")
 
 
 app.set("view engine", "ejs");
@@ -20,49 +21,74 @@ main().then(() => {
 })
 
 async function main() {
-    await mongoose.connect("mongodb://127.0.0.1:27017/whatsapp")
+    await mongoose.connect("mongodb://127.0.0.1:27017/fakewhatsapp")
 }
 
 // index route
 
 app.get("/chats", async (req, res) => {
-    let chats = await Chat.find();
-    //console.log(chats);
-    res.render("index.ejs", { chats });
-    // res.send("Chats show section");
+    try {
+        let chats = await Chat.find();
+        //console.log(chats);
+        res.render("index.ejs", { chats });
+        // res.send("Chats show section");
+    } catch (err) {
+        next(err);
+    }
+
 })
 
 // Form for create new route
 app.get("/chats/new", (req, res) => {
+    // throw new ExpressError(404,"form not found" );
     res.render("new.ejs")
     // res.send("New route");
 })
 
 // create route
-app.post("/chats", (req, res) => {
-    let { from, to, message } = req.body;
-    let newChat = new Chat({
-        from: from,
-        to: to,
-        message: message,
-        created_at: new Date(),
-    })
+app.post("/chats", async (req, res, next) => {
+    try {
+        let { from, to, message } = req.body;
+        let newChat = new Chat({
+            from: from,
+            to: to,
+            message: message,
+            created_at: new Date(),
+        })
 
-    newChat.save().then((res) => {
-        console.log(res);
-    })
-        .catch((err) => {
-            console.log(err);
-        });
+        await newChat.save();
+        res.redirect("/chats");
+    } catch (err) {
+        next(new ExpressError(401, "You have fill the required fields"));
+    }
 
-    res.redirect("/chats");
+})
+
+// NEW Show route--------
+app.get("/chats/:id", async (req, res, next) => {
+    try{
+        let { id } = req.params;
+    let chat = await Chat.findById(id);
+    if (!chat) {
+        next(new ExpressError(404, "chat not found"));
+    }
+    res.render("edit.ejs", { chat });
+    }catch(err){
+        next(new ExpressError(404, "you are trying to access different thing"));
+    }
+    
 })
 
 // edit form route
 app.get("/chats/:id/edit", async (req, res) => {
-    let { id } = req.params;
+    try{
+        let { id } = req.params;
     let chat = await Chat.findById(id);
     res.render("edit.ejs", { chat });
+    }catch(err){
+        next(new ExpressError(401, "the message is not edited"));
+    }
+    
 })
 
 // Update route
@@ -74,15 +100,15 @@ app.put("/chats/:id", async (req, res) => {
         // Update the message and set updated_at to the current date
         let updatedChat = await Chat.findByIdAndUpdate(
             id,
-            { 
-                message: newMessage, 
+            {
+                message: newMessage,
                 updated_at: new Date() // Correctly update the updated_at field
             },
             { runValidators: true, new: true } // Ensure validation and return the updated document
         );
 
         if (!updatedChat) {
-            return res.status(404).send("Chat not found");
+            next(new ExpressError(404, "chat not found"));
         }
 
         console.log("Updated Chat:", updatedChat);
@@ -94,15 +120,25 @@ app.put("/chats/:id", async (req, res) => {
 });
 
 // destroy route
-app.delete("/chats/:id", async (req, res) => {
-    let { id } = req.params;
-    let deletedChat = await Chat.findByIdAndDelete(id);
-    //console.log(deletedChat);
-    res.redirect("/chats");
+app.delete("/chats/:id", async (req, res, next) => {
+    try {
+        let { id } = req.params;
+        let deletedChat = await Chat.findByIdAndDelete(id);
+        //console.log(deletedChat);
+        res.redirect("/chats");
+    } catch (err) {
+        next(err);
+    }
+
 })
 
 app.get("/", (req, res) => {
     res.send("working");
+})
+
+app.use((err, req, res, next) => {
+    let { status = 500, message = "some error occured" } = err;
+    res.status(status).send(message);
 })
 
 app.listen(port, () => {
